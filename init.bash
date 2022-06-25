8bde081b37863c5192c2068606cdd259328cbb39
Santiago Souza <santiagosouzape@gmail.com>
2022-06-25 12:30:23 -0300

    Update init.bash

--- init.bash
+++ init.bash
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 installHomebrew() {
+  clear && printf '\e[3J'
   echo Installing Homebrew...
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   installZSH
 }
 
 installNodeJs() {
+  clear && printf '\e[3J'
   echo Installing NVM ...
   brew install nvm 
   nvm install node
 }
 
 installVSCODE() {
+  clear && printf '\e[3J'
   echo Installing vsCode ...
   brew install --cask visual-studio-code
 }
 
 installZSH() {
+  clear && printf '\e[3J'
   echo Installing zsh ...
   brew install zsh
   echo Installing Hyper terminal ...
