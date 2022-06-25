e340ef33e7592b0bf9677810e423dc31e37cce92
Santiago Souza <santiagosouzape@gmail.com>
2022-06-25 12:28:07 -0300

    Update init.bash

--- init.bash
+++ init.bash
@@ -1,2 +1,38 @@
+#!/bin/bash
 
-## Init
+installHomebrew() {
+  echo Installing Homebrew...
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  installZSH
+}
+
+installNodeJs() {
+  echo Installing NVM ...
+  brew install nvm 
+  nvm install node
+}
+
+installVSCODE() {
+  echo Installing vsCode ...
+  brew install --cask visual-studio-code
+}
+
+installZSH() {
+  echo Installing zsh ...
+  brew install zsh
+  echo Installing Hyper terminal ...
+  brew install --cask hyper
+  cp .zshrc ~/
+  installVSCODE
+}
+
+read -p "Install homebrew? (yes/no) " yn
+
+case $yn in 
+	yes ) installHomebrew;;
+	no ) installZSH;
+		exit;;
+	* ) echo invalid response;
+		exit 1;;
+esac
+
